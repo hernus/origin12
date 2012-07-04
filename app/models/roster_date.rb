ddf3ff7a63521627ca6a573b66f362bd0ca42b7b
Michael Bamford <michael@michaelbamford.com>
2012-07-05 01:54:42 +1000

    Duplicate RosterDay into a week

--- app/models/roster_date.rb
+++ app/models/roster_date.rb
@@ -12,12 +12,25 @@ class RosterDate < ActiveRecord::Base
 
   belongs_to :employee
 
-  has_many :rosters
+  has_many :rosters, :dependent => :destroy
  
   accepts_nested_attributes_for :rosters, allow_destroy: true, reject_if: proc { |attrs| attrs['project_id'].blank? }
 
   def build_shifts
     (MAXIMUM_SHIFTS - rosters.length).times { |shift| rosters.build shift: shift }
+  end
+
+  def duplicate(*date_strings)
+    self.class.transaction do
+      dates = date_strings.map { |date| Date.parse(date) }      
+      RosterDate.destroy_all(date: dates, employee_id: self.employee_id, locked: false)
+      dates.each do |date|
+        dup_roster_date = self.dup
+        dup_roster_date.date = date
+        dup_roster_date.rosters << rosters.map(&:dup)
+        dup_roster_date.save
+      end
+    end
   end
 
 end
