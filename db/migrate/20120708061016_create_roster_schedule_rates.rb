eb5cbc10012d8712e6b46b9581f539f99e13e25e
Michael Bamford <michael@michaelbamford.com>
2012-07-10 20:51:47 +1000

    Removed obsoluete roster schedule rates

--- db/migrate/20120708061016_create_roster_schedule_rates.rb
+++
@@ -1,9 +0,0 @@
-class CreateRosterScheduleRates < ActiveRecord::Migration
-  def change
-    create_table :roster_schedule_rates do |t|
-      t.integer :roster_id, null: false
-      t.integer :schedule_rate_id, null: false
-      t.timestamps
-    end
-  end
-end
